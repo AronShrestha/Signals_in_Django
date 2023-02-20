8524ced5f5d943da59582958b91e5ea1682349bb
AronShrestha <aronshrestha498@gmail.com>
2023-02-20 15:49:36 +0545

    Signals in django for transaction-usecase

---
+++ orders/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from cars.models import Car
+
+# Create your models here.
+class Order(models.Model):
+    name = models.CharField(max_length= 200)
+    cars = models.ManyToManyField(Car)
+    total = models.PositiveBigIntegerField(blank = True,null=True)
+    total_price = models.PositiveBigIntegerField(blank =True,null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f"name : {self.name}"
